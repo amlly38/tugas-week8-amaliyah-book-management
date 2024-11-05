4f6cec76ad948dee0452f701e6badbe617aa6ffc
amlly38 <amaliyahamel0308@gmail.com>
2024-11-05 22:26:33 +0700

    feat(routes): set up book route with CRUD endpoints

---
+++ src/routes/book_routes.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const BookController = require("../controllers/book_controller")
+
+const bookRouter = express.Router()
+
+bookRouter.post("/book", BookController.create)
+bookRouter.get("/books", BookController.getAll)
+bookRouter.get("/book/:id", BookController.getById)
+bookRouter.put("/book/:id", BookController.update)
+bookRouter.delete("/book/:id", BookController.delete)
+bookRouter.post("/book/upload", BookController.uploadImage)
+
+
+module.exports = bookRouter
