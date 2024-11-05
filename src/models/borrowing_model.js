ae98d60b879426629bdbb944a35c05d92f76641f
amlly38 <amaliyahamel0308@gmail.com>
2024-11-05 22:20:34 +0700

    feat(models): add BorrowingSchema

---
+++ src/models/borrowing_model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const BorrowingSchema = new mongoose.Schema({
+    bookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true
+    },
+    borrowerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Borrower',
+      required: true
+    },
+    borrowDate: {
+      type: Date,
+      required: true,
+      default: Date.now
+    },
+    dueDate: {
+      type: Date,
+      required: true
+    },
+    returnDate: {
+      type: Date,
+      default: null
+    },
+    lateFee: { 
+      type: Number,
+      default: 0,
+      min: 0 
+    },
+    status: {
+      type: String,
+      enum: ['ACTIVE', 'RETURNED'],
+      default: 'ACTIVE'
+    }
+  },
+);
+  
+  module.exports = BorrowingSchema
--- src/models/index.js
+++ src/models/index.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose')
 const CategorySchema = require("./category_model");
 const BorrowerSchema = require("./borrower_model");
 const AuthorSchema = require("./author_model");
+const BookSchema = require("./book_model");
+const BorrowingSchema = require("./borrowing_model");
 
 module.exports = {
     Category: mongoose.model('Category', CategorySchema),
     Borrower: mongoose.model('Borrower', BorrowerSchema),
-    Author: mongoose.model('Author', AuthorSchema)
+    Author: mongoose.model('Author', AuthorSchema),
+    Book: mongoose.model('Book', BookSchema),
+    Borrowing: mongoose.model('Borrowing', BorrowingSchema)
+
 }
